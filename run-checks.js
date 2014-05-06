2db4eeb33f5d546a9cb5fc143a3da8ee81aa6f0e
Atul Varma <varmaa@gmail.com>
2014-05-06 17:50:31 -0400

    run up to 5 checks simultaneously.

--- run-checks.js
+++ run-checks.js
@@ -5,13 +5,14 @@ var async = require('async');
 var printf = require('printf');
 
 var CHECKS_DIR = path.join(__dirname, 'checks');
+var PROCESS_LIMIT = 5;
 var TIMEOUT = 40000;
 var JS_RE = /^(.+)\.js$/;
 var CHECK_FILENAMES = fs.readdirSync(CHECKS_DIR).filter(function(filename) {
   return JS_RE.test(filename);
 }).sort();
 
-async.mapSeries(CHECK_FILENAMES, function(filename, cb) {
+async.mapLimit(CHECK_FILENAMES, PROCESS_LIMIT, function(filename, cb) {
   var basename = filename.match(JS_RE)[1];
   var abspath = path.join(CHECKS_DIR, filename);
   var ended = false;
@@ -22,7 +23,8 @@ async.mapSeries(CHECK_FILENAMES, function(filename, cb) {
     clearTimeout(timeout);
     if (typeof(code) == 'number')
       code = code === 0 ? 'OK' : 'ERROR';
-    printf(process.stdout, code + '\n');
+
+    printf(process.stdout, '%-50s %s\n', basename, code);
     cb(null, code);
   };
   var timeout = setTimeout(function() {
@@ -34,7 +36,6 @@ async.mapSeries(CHECK_FILENAMES, function(filename, cb) {
   });
 
   child.on('close', onClose);
-  printf(process.stdout, '%-50s', basename);
 }, function(err, codes) {
   if (err) throw err;
 
