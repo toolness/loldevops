6284dfc755887c84440b8bbc515f04aadfdaf076
Atul Varma <varmaa@gmail.com>
2015-02-03 17:59:13 -0500

    added checks for mari and jess loveboms

---
+++ checks/get-well-soon-jess.herokuapp.com.js
@@ -0,0 +1,3 @@
+var verify200 = require('../lib/verify-200');
+
+verify200('http://get-well-soon-jess.herokuapp.com/', /Get Well Soon, Jess/);
---
+++ checks/mari-rocks.herokuapp.com.js
@@ -0,0 +1,3 @@
+var verify200 = require('../lib/verify-200');
+
+verify200('http://mari-rocks.herokuapp.com/', /We Love and Will Miss You, Mari/);
